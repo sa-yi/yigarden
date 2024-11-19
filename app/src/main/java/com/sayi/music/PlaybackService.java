dde5bdfa1fcb9b3e291908cdb9b4681b9cfa4ff9
Sayi <2559806259@qq.com>
2024-11-19 21:20:13 +0800

    移除了不稳定的api

--- app/src/main/java/com/sayi/music/PlaybackService.java
+++ app/src/main/java/com/sayi/music/PlaybackService.java
@@ -13,24 +13,20 @@ import androidx.media3.session.MediaSession;
 import androidx.media3.session.MediaSessionService;
 
 import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.common.util.concurrent.SettableFuture;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@UnstableApi
+
 public class PlaybackService extends MediaSessionService {
     private MediaSession mediaSession = null;
-    private MediaSessionCallback mediaSessionCallback;
-    @OptIn(markerClass = UnstableApi.class) @Override
+    @Override
     public void onCreate() {
         super.onCreate();
-        ExoPlayer player = new ExoPlayer.Builder(this).build();
-        mediaSessionCallback=new MediaSessionCallback();
-        mediaSession = new MediaSession.Builder(this, player).setCallback(mediaSessionCallback).build();
-
         Intent intent=new Intent(this,MusicActivity.class);
-        mediaSession.setSessionActivity(PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_IMMUTABLE));
+        ExoPlayer player = new ExoPlayer.Builder(this).build();
+        mediaSession = new MediaSession.Builder(this, player).setSessionActivity(PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT|PendingIntent.FLAG_IMMUTABLE)).setCallback(new MediaSession.Callback() {}).build();
+
     }
 
     @Nullable
@@ -45,24 +41,5 @@ public class PlaybackService extends MediaSessionService {
         mediaSession.release();
         mediaSession = null;
         super.onDestroy();
-    }
-    @UnstableApi public class MediaSessionCallback implements MediaSession.Callback {
-        @NonNull
-        @Override
-        public ListenableFuture<MediaSession.MediaItemsWithStartPosition> onPlaybackResumption(@NonNull MediaSession mediaSession, @NonNull MediaSession.ControllerInfo controller) {
-
-            SettableFuture<MediaSession.MediaItemsWithStartPosition> settableFuture = SettableFuture.create();
-            settableFuture.addListener(() -> {
-                // Your app is responsible for storing the playlist and the start position
-                // to use here
-                MediaSession.MediaItemsWithStartPosition resumptionPlaylist = restorePlaylist();
-                settableFuture.set(resumptionPlaylist);
-            }, MoreExecutors.directExecutor());
-            return settableFuture;
-        }
-    }
-    @OptIn(markerClass = UnstableApi.class)
-    private MediaSession.MediaItemsWithStartPosition restorePlaylist() {
-        return new MediaSession.MediaItemsWithStartPosition(MusicScanner.mediaItems, 0, 0);
     }
 }
